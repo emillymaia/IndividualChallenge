0f7da4afacd364bfe0509142831affdce9b5c597
Emilly Maia <emillypereira1290@gmail.com>
2022-10-11 08:43:14 -0300

    Initial Commit

---
+++ IndividualChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IndividualChallenge
+//
+//  Created by Emilly Maia on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
